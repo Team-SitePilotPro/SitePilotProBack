e07165a937ac688b76ae01751c0201125f201d1e
Rmsaone <rmsaone@gmail.com>
2026-04-25 22:41:14 +0200

    feat(adress): add address model, migrate

--- database/migrations/2026_04_17_220222_create_addresses_table.php
+++ database/migrations/2026_04_17_220222_create_addresses_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('worksites', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->string('street');
+            $table->string('city');
+            $table->integer('zip_code');
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('worksites');
+        Schema::dropIfExists('addresses');
     }
 };
